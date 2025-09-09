3f9d9c54bb9fa030fb34ed9d3f7d2227e9334e46
Eyüp Karataş <eyupkaratasbusiness@gmail.com>
2025-09-09 23:14:15 +0300

    commit

--- src/components/inputs/CheckboxWithLabel.tsx
+++ src/components/inputs/CheckboxWithLabel.tsx
@@ -33,7 +33,7 @@ export function CheckboxWithLabel<S>({
       name={nameInSchema}
       render={({ field }) => (
         <FormItem className="w-full flex items-center gap-2">
-          <FormLabel className="text-base w-1/3 mt-2" htmlFor={nameInSchema}>
+          <FormLabel className="text-base w-1/3" htmlFor={nameInSchema}>
             {fieldTitle}
           </FormLabel>
 
